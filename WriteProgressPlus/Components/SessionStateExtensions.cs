b9edd438900785a3af3ef76e19d51e42b5258ce9
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-03-19 19:18:18 +0100

    Added extensions for SessionState

---
+++ WriteProgressPlus/Components/SessionStateExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace WriteProgressPlus.Components;
+internal static class SessionStateExtensions
+{
+    public static object GetVariable(this SessionState state, string name, object defaultValue)
+    {
+        return state.PSVariable.GetValue(name, defaultValue);
+    }
+
+    public static object GetVariable(this SessionState state, string name)
+    {
+        return state.PSVariable.GetValue(name);
+    }
+
+    public static T? GetVariable<T>(this SessionState state, string name)
+    {
+        return (T)state.GetVariable(name);
+    }
+
+    public static T GetVariable<T>(this SessionState state, string name, T defaultValue)
+    {
+        return (T)state.GetVariable(name, defaultValue);
+    }
+}
