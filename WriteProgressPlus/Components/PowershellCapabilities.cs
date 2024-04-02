b5d101001e336f3a46c8829d59cf0793fa39a0b8
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-04-02 22:43:31 +0200

    use capabilities

---
+++ WriteProgressPlus/Components/PowershellCapabilities.cs
@@ -0,0 +1,9 @@
+﻿namespace WriteProgressPlus.Components;
+
+public class PowershellCapabilities
+{
+    private static readonly Lazy<PowershellCapabilities> instance = new Lazy<PowershellCapabilities>(() => new PowershellCapabilities());
+    public static PowershellCapabilities Instance => instance.Value;
+    public bool CanThrottleProgressUpdates { get; private set; }
+    public bool HasMinimalStyle { get; private set; }
+}
